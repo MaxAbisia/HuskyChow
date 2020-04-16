a03f0bd0f1cc37c752705faff185e27302a8064c
maxabisia <abisia.m@husky.neu.edu>
2020-04-16 10:01:39 -0700

    Added share function

--- app/src/main/java/com/example/huskychow/DetailActivity.java
+++ app/src/main/java/com/example/huskychow/DetailActivity.java
@@ -1,9 +1,11 @@
 package com.example.huskychow;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,6 +24,14 @@ public class DetailActivity extends FragmentActivity implements OnClickListener 
     private TextView address;
     private TextView url;
     private ImageButton backButton;
+    private Button navButton;
+    private Button shareButton;
+
+    GlobalVariables g;
+
+
+    String resAddress = "";
+    String resName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +49,13 @@ public class DetailActivity extends FragmentActivity implements OnClickListener 
         backButton = findViewById(R.id.backbutton);
         backButton.setOnClickListener(this);
 
-        GlobalVariables g = (GlobalVariables) getApplication();
+        navButton = findViewById(R.id.navigateButton);
+        navButton.setOnClickListener(this);
+
+        shareButton = findViewById(R.id.shareButton);
+        shareButton.setOnClickListener(this);
+
+        g = (GlobalVariables) getApplication();
 
         // if restaurant is selected, set the detail info
         for (Restaurant res : restaurantList.getRestaurants()) {
@@ -81,6 +97,54 @@ public class DetailActivity extends FragmentActivity implements OnClickListener 
             case R.id.backbutton:
                 this.startActivity(new Intent(DetailActivity.this, MapsActivity.class));
                 this.finish();
+                break;
+
+            case R.id.navigateButton:
+                goMaps();
+                break;
+
+            case R.id.shareButton:
+                goShare();
+                break;
         }
+    }
+
+
+    public void goMaps() {
+
+        for (Restaurant res : restaurantList.getRestaurants()) {
+            if (g.getValue().equals(res.getName())) {
+                resAddress = res.getAddress();
+                resName = res.getName();
+            }
+        }
+
+        // Create a Uri from an intent string. Use the result to create an Intent.
+        Uri gmmIntentUri = Uri.parse("geo:0,0?q=" + resName + resAddress + " Boston, MA");
+
+        // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        // Make the Intent explicit by setting the Google Maps package
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        // Attempt to start an activity that can handle the Intent
+        startActivity(mapIntent);
+    }
+
+    public void goShare() {
+
+        for (Restaurant res : restaurantList.getRestaurants()) {
+            if (g.getValue().equals(res.getName())) {
+                resAddress = res.getAddress();
+                resName = res.getName();
+            }
+        }
+
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        String shareBody = "Here's the Address for " + resName + ":\n" + resAddress;
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+        startActivity(Intent.createChooser(sharingIntent, "Share via"));
     }
 }
